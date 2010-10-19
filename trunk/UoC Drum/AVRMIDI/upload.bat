8e98c96754d1076dce1acf8c39b52c02a9bdfb80
adrian.gin <adrian.gin@15d4e0f2-5640-0410-9928-f923d4cf3eeb>
2010-10-19 19:38:03 +0000

    added debug

--- trunk/UoC Drum/AVRMIDI/upload.bat
+++ trunk/UoC Drum/AVRMIDI/upload.bat
@@ -2,6 +2,6 @@ cd default
 make clean
 make
 avr-objcopy -Iihex AVRMIDI.hex -Obinary AVRMIDI.bin
-REM avrdude -patmega8 -e -U flash:w:AVRMIDI.hex -c ftisp -P ft0
-ftisp -E -fw AVRMIDI.bin
+REM avrdude -B 9600 -patmega8 -e -U flash:w:AVRMIDI.hex -c ftisp -P ft0
+ftisp -B 115200 -E -fw AVRMIDI.bin
 cd ..
