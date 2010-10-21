35dbf8346322ecc4ce1771ca1f7deccffec7325e
adrian.gin <adrian.gin@15d4e0f2-5640-0410-9928-f923d4cf3eeb>
2010-10-21 10:09:05 +0000

    Cleanup of code.
    AVR-MIDI 1.0 Initial Release!
    This uses bulk transfers.
    About 3kB/s for transferring to PC
    about 2.5kB/s receiving from USB PC.

--- UoC Drum/AVRMIDI/main.c
+++ UoC Drum/AVRMIDI/main.c
@@ -21,42 +21,7 @@
 #include "MIDICodes/MIDICodes.h"
 
 
- 
 
- /* Needs to be a power of 2 */
-#define MIDI_OUT_BUFFER    (128)
-#define MIDI_OUT_MASK      (MIDI_OUT_BUFFER - 1)
-#define SYS_COMMON_MSG (22)
-
-/* Consider all the rest to be 1-Byte System Common Messages */
-uint8_t MIDILookupTable[] PROGMEM =
-{
-   // Start of Voice Messages
-   0x80, 3, 0x08,
-   0x90, 3, 0x09,
-   0xA0, 3, 0x0A,
-   0xB0, 3, 0x0B,
-   0xC0, 2, 0x0C,
-   0xD0, 2, 0x0D,
-   0xE0, 3, 0x0E,
-   0x00,       
-   // Start of System Common Messages
-   0xF0, 3, 0x04,
-   0xF1, 2, 0x02,
-   0xF2, 3, 0x03,
-   0xF3, 2, 0x02,
-   0x00
-};
-
-uint8_t wMIDImsgCount = 0;
-uint8_t rMIDImsgCount = 0; 
-
-/* Hold enough data for a few messages,
- */
-static usbMIDIMessage_t MIDImsgComplete[MIDI_OUT_BUFFER];
-
- 
- 
 // This descriptor is based on http://www.usb.org/developers/devclass_docs/midi10.pdf
 // 
 // Appendix B. Example: Simple MIDI Adapter (Informative)
@@ -193,7 +158,7 @@ static PROGMEM char configDescrMIDI[] = {	/* USB configuration descriptor */
 	9,			/* bLenght */
 	USBDESCR_ENDPOINT,	/* bDescriptorType = endpoint */
 	0x1,			/* bEndpointAddress OUT endpoint number 1 */
-	3,			/* bmAttributes: 2:Bulk, 3:Interrupt endpoint */ //This should be interrupt so the USB is slower than the UART.
+	2,			/* bmAttributes: 2:Bulk, 3:Interrupt endpoint */ //This should be bulk for max speed
 	8, 0,			/* wMaxPacketSize */
 	2,			/* bIntervall in ms */
 	0,			/* bRefresh */
@@ -284,15 +249,7 @@ uchar usbFunctionSetup(uchar data[8])
 /* We're never asked to send lots of data, so lets not send anything */
 uchar usbFunctionRead(uchar * data, uchar len)
 {
-	data[0] = 0;
-	data[1] = 0;
-	data[2] = 0;
-	data[3] = 0;
-	data[4] = 0;
-	data[5] = 0;
-	data[6] = 0;
-	return 7;
-   
+	return 0;   
 }
  
  
@@ -306,8 +263,6 @@ uchar usbFunctionWrite(uchar * data, uchar len)
 {
    return 1;
 }
- 
- 
  
  
 /*---------------------------------------------------------------------------*/
@@ -324,23 +279,18 @@ void usbFunctionWriteOut(uchar * data, uchar len)
 
    /* Route it all to the UART port at 31250 baud */
    byteCount = usbMIDI_ParseData(data, len);
-
+   uartTxDump(data, byteCount);
    /* Here if the TxBuffer cannot hold the new data, we should disable all 
     * USB requests and wait for the MIDI TxBuffer to catch up.
     * Since we need to process the next request, we have to disable BEFORE
     * the buffer will be full (hence the 16).
     *
-    * We shall re-enable the request in the main loop, not the interrupt.    
-   /* This is required if our endpoints are interrupt based, note that you can't
-    * DisableAllRequests on a bulk transfer. */*/
-   if( (ringbuffer_len( (RINGBUFFER_T*)&TransmitBuffer ) + 16) >= (TXBUFFER_SIZE-1) )
+    * We shall re-enable the request in the main loop, not the interrupt.  */
+   if( (ringbuffer_len( (RINGBUFFER_T*)&TransmitBuffer ) + 8) >= (TXBUFFER_SIZE-1) )
    {
       usbDisableAllRequests();
    }
 
-
-   uartTxDump(data, byteCount);
-   
    /* for internal loopback */
    /*for(uint8_t i = 0 ; i < byteCount ; i++ )
    {
@@ -350,20 +300,11 @@ void usbFunctionWriteOut(uchar * data, uchar len)
    /* This shouldn't happen since the USB output is quite fast (bulk) */
    /* This is required if our endpoints are interrupt based, note that you can't
     * DisableAllRequests on a bulk transfer. */
-   if( ((wMIDImsgCount + 1) & MIDI_OUT_MASK) == ((rMIDImsgCount) & MIDI_OUT_MASK) )
+   if( !usbMIDI_bufferIsReady() )
    {
       usbDisableAllRequests();
    }
 }
- 
-
- 
-
-
-
- 
-
- 
  
  
 ISR(SIG_UART_RECV)
@@ -376,178 +317,19 @@ ISR(SIG_UART_RECV)
 }
  
  
- 
- 
- 
-/* Given the first MIDI Message, we return the
- * corresponding index value in MIDILookupTable
- * Returns: UNKNOWN_VOICE_MSG or UNKNOWN_SYS_MSG;
- * if it does not exist in the table */
-uint8_t LookupMIDIMessage(uint8_t MIDIStatusCode)
-{
-   uint8_t i;
-   /* System Common Message */
-   if( MIDIStatusCode >= 0xF0 )
-   {
-      for( i = SYS_COMMON_MSG; pgm_read_byte(&MIDILookupTable[i]) ; i = i + 3 )
-      {
-         if( MIDIStatusCode == pgm_read_byte(&MIDILookupTable[i]) )
-         {
-            return i;
-         }
-      }
-      return UNKNOWN_SYS_MSG;
-   }
-   else
-   {
-      MIDIStatusCode = (MIDIStatusCode & 0xF0);
-      for( i = 0; pgm_read_byte(&MIDILookupTable[i]) ; i = i + 3 )
-      {
-         if( MIDIStatusCode == pgm_read_byte(&MIDILookupTable[i]) )
-         {
-            return i;
-         }
-      }
-      return UNKNOWN_VOICE_MSG;
-   }
-}
- 
-/* Returns the CIN number */
-uint8_t GetCINNumber(uint8_t messageIndex)
-{
-   if( messageIndex == UNKNOWN_SYS_MSG)
-   {
-        return 0x05;
-   }
-   uint8_t CIN = pgm_read_byte(&MIDILookupTable[messageIndex+2]);
-   /* Most system common messages are 1 byte long and
-    * therefore have a CIN of 0x05 */
-   return CIN;
-}
- 
- 
-uint8_t GetNumberOfBytesToRead(uint8_t messageIndex)
-{
-   if( messageIndex == UNKNOWN_SYS_MSG)
-   {
-        return 1;
-   }
-   uint8_t bytesToReturn = pgm_read_byte(&MIDILookupTable[messageIndex+1]);
-   /* Most system common messages are 1 byte long */
-   return bytesToReturn;
-}
- 
- 
- 
 
-/* When MIDI Data is ready
- * The function will return MIDI_DATA_READY and the
- * USB_MIDI packet will be in MIDIMessage */
-uint8_t MIDIDataReady(uint8_t inByte, usbMIDIMessage_t* MIDIMessage)
-{
-   /* You can technically have two midi events per midi message, but lets just
-    * keep it to one.
-    */
-   static uint8_t byteCount = 0;
-   static uint8_t bytesToReceive = 0;
-   static uint8_t receivingSysEx = 0;
-   static uint8_t CIN = 0;
-   uint8_t messageIndex = 0;
- 
-   /* Check for input errors */
-   if( (byteCount == 0) && (inByte <= MIDI_MAX_DATA) && (!receivingSysEx))
-   {
-      byteCount = 0;
-      return MIDI_DATA_NOT_READY;
-   }
-   /* A new status byte is received, discard the old one */
-   if( (byteCount > 0) && (inByte > MIDI_MAX_DATA) && (inByte != MIDI_SYSEX_STOP))
-   {
-      byteCount = 0;
-      receivingSysEx = 0;
-   }
- 
- 
-   if( inByte == MIDI_SYSEX_START )
-   {
-       receivingSysEx = 1;
-   }
- 
-   /* Determine how many bytes to receive based on the 'first'
-    * byte (MIDI_Status Code) */
-   if( byteCount == 0 )
-   {
- 
-      /* Clear */
-      MIDIMessage->header = 0;
-      MIDIMessage->MIDIData[0] = 0;
-      MIDIMessage->MIDIData[1] = 0;
-      MIDIMessage->MIDIData[2] = 0;
- 
-      if( !receivingSysEx )
-      {
-          messageIndex = LookupMIDIMessage(inByte);
-          bytesToReceive = GetNumberOfBytesToRead(messageIndex);
-          CIN = GetCINNumber(messageIndex);
-          MIDIMessage->header = CIN;
-      }
-   }
- 
-   MIDIMessage->MIDIData[byteCount] = inByte;
-   byteCount++;
- 
-   if( receivingSysEx )
-   {
-      /* By default we are continuing a SysEx Message */
-      bytesToReceive = 3;
-      MIDIMessage->header = 0x04;
- 
-      /* If we recieve a SYSEX STOP then send the message */
-      if( inByte == MIDI_SYSEX_STOP )
-      {
-         bytesToReceive = byteCount;
-         MIDIMessage->header = byteCount + 0x04;
-         receivingSysEx = 0;
-      }
-   }
- 
-   if( byteCount == bytesToReceive)
-   {
-      byteCount = 0;
-      return MIDI_DATA_READY;
-   }
-
-   return MIDI_DATA_NOT_READY;
-}
- 
- 
- 
- 
-
- 
 int main(void)
 {
    uint8_t midiReady = 0;
    uint8_t outputBuffer[8];
    
    usbInit();
-   uint8_t temp[] = {0x05, 0xF8, 0x00, 0x00, 0x05, 0xF8, 0x00, 0x00};
  
 /* Just use 31250 baud */
 /* Formula = ((Hz / Baud) / 16) - 1 */
    uartInit(39, 0);
  
    sei();
-
-   /*while(1)
-   {
-      usbPoll();
-       if( usbInterruptIsReady() )
-       {
-             usbSetInterrupt( (uint8_t*)temp, 8);
-        }
-   }*/
-
 
    while(1)
    { /* main event loop */
@@ -558,17 +340,18 @@ int main(void)
         * UART Speed < USB Inspeed (likely under bulk mode, but unlikely under interrupt mode */
        /* To save the USB output buffer from overflowing, due to a
         * UART Speed > USB Outspeed (not likely under bulk mode, but likely under interrupt mode */
-       if( usbAllRequestsAreDisabled() && ((wMIDImsgCount+ 1) & MIDI_OUT_MASK) != ((rMIDImsgCount) & MIDI_OUT_MASK)
-            && (ringbuffer_len( (RINGBUFFER_T*)&TransmitBuffer ) + 16) < (TXBUFFER_SIZE-1))
+       if(  usbAllRequestsAreDisabled() 
+            && usbMIDI_bufferIsReady()
+            && (ringbuffer_len( (RINGBUFFER_T*)&TransmitBuffer ) + 8) < (TXBUFFER_SIZE-1))
        {
          usbEnableAllRequests();
        }
 
-      /* Process messages in the buffer is there are any */
+      /* Process messages in the UART Rx buffer is there are any */
        if( ringbuffer_len((RINGBUFFER_T*)&ReceiveBuffer) )
        {
           /* Only process this stuff if we have enough room in the MIDI out buffer */
-          if( ((wMIDImsgCount + 1) & MIDI_OUT_MASK) != rMIDImsgCount)
+          if( usbMIDI_bufferIsReady() )
           {
              uint8_t nextByte = ringbuffer_get((RINGBUFFER_T*)&ReceiveBuffer);
              midiReady = MIDIDataReady(nextByte, &MIDImsgComplete[wMIDImsgCount]);
@@ -580,29 +363,28 @@ int main(void)
           }
        }
  
-
-       if( (rMIDImsgCount != wMIDImsgCount) && usbInterruptIsReady() )
+       /* If there is device => USB waiting to be sent */
+       if( usbMIDI_bufferLen() && usbInterruptIsReady() )
        {
        /* If the USB Interrupt isn't ready, then we have an issue
         * of data coming from the UART quicker than it is going out, to keep up
         * we need to send 2 data events sometimes */
-          if( ((rMIDImsgCount + 1) & MIDI_OUT_MASK) != ((wMIDImsgCount) & MIDI_OUT_MASK) )
+          if( usbMIDI_bufferLen() == 1 )
           {
+             /* Send 1 byte */
+             usbSetInterrupt( (uint8_t*)&MIDImsgComplete[rMIDImsgCount], 4);
+             rMIDImsgCount = (rMIDImsgCount + 1) & MIDI_OUT_MASK;
+          }
+          else
+          {
+             /* Send 2 bytes */
              memcpy(&outputBuffer, &MIDImsgComplete[rMIDImsgCount], sizeof(usbMIDIMessage_t));
              memcpy(&outputBuffer[4], &MIDImsgComplete[(rMIDImsgCount+1) & MIDI_OUT_MASK], sizeof(usbMIDIMessage_t));
              usbSetInterrupt( (uint8_t*)&outputBuffer, 8);
              rMIDImsgCount = (rMIDImsgCount + 2) & MIDI_OUT_MASK;
-          }
-          else
-          {
-             usbSetInterrupt( (uint8_t*)&MIDImsgComplete[rMIDImsgCount], 4);
-             rMIDImsgCount = (rMIDImsgCount + 1) & MIDI_OUT_MASK;
-          }
-       }
-       
-       
 
-              
+          }
+       }              
    }
    return 0;
 }
