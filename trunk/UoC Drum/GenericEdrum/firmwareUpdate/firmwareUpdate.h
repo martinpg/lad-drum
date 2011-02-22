03eb1568f8ee15d3c70d0ed7dfeab9ab5ad55a6d
adrian.gin <adrian.gin@15d4e0f2-5640-0410-9928-f923d4cf3eeb>
2011-02-22 07:05:03 +0000

    WIP

---
+++ trunk/UoC Drum/GenericEdrum/firmwareUpdate/firmwareUpdate.h
@@ -0,0 +1,17 @@
+/* 20/02/2011 EDrum
+ * Allows the updating of Firmware over SysEx messages */
+
+#ifndef 	_FIRMWAREUPDATE_H
+#define	_FIRMWAREUPDATE_H
+
+
+#define	UART_STATUS_REG	UCSRA
+#define	TRANSMIT_COMPLETE_BIT	TXC
+#define	RECEIVE_COMPLETE_BIT	RXC
+#define	UART_DATA_REG	UDR
+
+void ReceiveFirmware(void);
+void FirmwareUpdateError();
+void ParseFirmwareData(uint8_t nextByte);
+
+#endif
